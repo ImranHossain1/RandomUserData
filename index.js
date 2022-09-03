c5297bb88683a4c1258d9790a65df4465dcaef18
ImranHossain1 <imranhossain1402@gmail.com>
2022-09-03 20:21:06 +0200

    error updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const userRouter = require('./routes/users.route');
+const errorHandler = require('./middleware/errorHandler');
 const app = express();
 const port = 5000;
 
@@ -12,7 +13,19 @@ app.use('/users',userRouter)
 app.get('/', (req, res) => {
   res.send('Hello from Assignment 1!')
 })
+app.all('*', (req,res)=>{
+  res.send("No Route Found.")
+})
+
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Assignment 1 listening on port ${port}`)
 })
+
+process.on('unhandledRejection', (error)=>{
+  console.log(error.name, error.message);
+  app.close(()=>{
+    process.exit(1);
+  })
+} )

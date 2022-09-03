f783c69e392a6e29e90ba769aa687f699ed1cc81
ImranHossain1 <imranhossain1402@gmail.com>
2022-09-03 20:24:12 +0200

    error updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const userRouter = require('./routes/users.route');
 const errorHandler = require('./middleware/errorHandler');
 const app = express();
-const port = 5000;
+const port =process.env.PORT||  5000;
 
 
 app.use(cors());
